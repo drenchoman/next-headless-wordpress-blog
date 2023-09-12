d33b2eb27c3a03fead8b5b9bf09b6566f12d8374
Oscar Harron <oscarharron@hotmail.com>
2023-09-12 21:19:00 +1200

    add graphql fetchAPI func

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image';
+const API_URL = process.env.NEXT_PUBLIC_WORDPRESS_API_ENDPOINT;
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
+      <p>{API_URL}</p>
     </main>
   );
 }
